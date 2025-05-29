34019ff5225a5b380cd55825e968f43a8c069811
Administrator <Administrator@10.16.24.249>
2025-05-29 11:26:20 -0400

    test

--- core/src/main/java/io/github/convicted_cogs_contest/TitleScreen.java
+++ core/src/main/java/io/github/convicted_cogs_contest/TitleScreen.java
@@ -1,6 +1,8 @@
 package io.github.convicted_cogs_contest;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 
 /** First screen of the application. Displayed after the application is created. */
 public class TitleScreen implements Screen {
@@ -12,6 +14,8 @@ public class TitleScreen implements Screen {
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(-1.f, 1.f, 1.f, -1.f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
