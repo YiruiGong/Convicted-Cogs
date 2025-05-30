c15ec9979868aa5d022220bf26a1a97bd86d6f2b
Administrator <Administrator@10.16.24.249>
2025-05-30 10:47:09 -0400

    test

--- core/src/main/java/io/github/convicted_cogs_contest/TitleScreen.java
+++ core/src/main/java/io/github/convicted_cogs_contest/TitleScreen.java
@@ -4,26 +4,37 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** First screen of the application. Displayed after the application is created. */
 public class TitleScreen extends ApplicationAdapter {
     SpriteBatch batch;
+    BitmapFont font;
+    BitmapFont toFightScreen;
     Texture Cog;
+    
     
     @Override
     public void create() {
         batch = new SpriteBatch();
         Cog = new Texture("cog.png");
+        font = new BitmapFont();
+        toFightScreen = new BitmapFont();
+        font.getData().scale(1f);
+        toFightScreen.getData().scale(1f);
     }
 
     @Override
     public void render() {
         // Draw your screen here. "delta" is the time since last render in seconds.
-        Gdx.gl.glClearColor(-1.f, 1.f, 1.f, -1.f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(Cog, 100, 100);
+        Gdx.gl.glClearColor(0.3f,0,0,1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        font.draw(batch, "Convicted Cogs Contest",800, 450);
+        toFightScreen.draw(batch, "Press space bar to Continue", 800, 200);
         batch.end();
     }
 
@@ -32,5 +43,6 @@ public class TitleScreen extends ApplicationAdapter {
         // Destroy screen's assets here.
         batch.dispose();
         Cog.dispose();
+        font.dispose();
     }
 }
