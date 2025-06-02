f0adc803c2c75bc40178f2fcb6ee40b22ea1d2c5
Administrator <Administrator@10.16.24.249>
2025-06-02 11:51:26 -0400

    test

--- core/src/main/java/io/github/convicted_cogs_contest/ControlScreen.java
+++ core/src/main/java/io/github/convicted_cogs_contest/ControlScreen.java
@@ -4,10 +4,41 @@
  */
 package io.github.convicted_cogs_contest;
 
+import com.badlogic.gdx.Screen;
+
 /**
  *
  * @author gongm
  */
-public class ControlScreen {
+public class ControlScreen implements Screen {
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float f) {
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+        
+    }
+
+    @Override
+    public void dispose() {
+    }
     
 }
